f9b350e1f6a7b663ebd2d55a329774412b1b2f4a
Druggist <m.najdora@gmail.com>
2016-05-20 10:46:04 +0200

    Prolog numeric lists

--- prolog-exercises/listy.pl
+++ prolog-exercises/listy.pl
@@ -94,3 +94,81 @@ append(WY1, WY2, WE), WY1=[_].  -dekompozycja listy, WY1 jednoelementowe
 append(_,[X],L). ostatni
 append(_,[X],L). 
 */
+
+tesame([H1],[H1],[]).
+tesame([H1,H2|T],[H1],[H2|T]) :-
+	H1 \= H2.
+tesame([H,H|T], [H|X], Y) :- 
+	tesame([H|T], X, Y).
+
+podzial([],[]).
+podzial(We, [X1|X2]) :-
+	tesame(We, X1, Y),
+	podzial(Y, X2).
+
+
+%PRZETWARZANIE NUMERYCZNE
+sumuj([X],X).
+sumuj([X|T],S) :-
+	sumuj(T, S1),
+	S is S1 + X.
+
+rosnacy([_]).
+rosnacy([X1,X2|T]) :-
+	X1 < X2,
+	rosnacy([X2|T]).
+
+arytmetyczny([_]). 
+arytmetyczny([_,_]). 
+arytmetyczny([X1,X2,X3|T]) :-
+	X1 - X2 =:= X2 - X3,
+	arytmetyczny([X2,X3|T]).
+
+maksio([X], X).
+maksio([X1,X2|T], X) :-
+	X1 >= X2,
+	maksio([X1|T], X).
+maksio([X1,X2|T], X) :-
+	X1 < X2,
+	maksio([X2|T], X).
+
+faster_maksio(X, Y) :-
+	max_list(X, Y).
+
+nstart(Y,0,[],Y).
+nstart([H|T],N,[H|X],Y) :-
+	N1 is N - 1,
+	nstart(T,N1,X,Y).
+
+rozbijaj([],[],[]).
+rozbijaj(We,[N|T],[[X]|Wy]) :-
+	nstart(We,N,X,Y),
+	rozbijaj(Y,T,Wy).
+
+
+silnia(1,1).
+silnia(N,F) :-
+	N1 is N - 1,
+	silnia(N1, F1),
+	F is F1 * N.
+
+pnp([],[],[]).
+pnp([H|T],[H|P],NP) :-
+	H mod 2 =:= 0,
+	pnp(T,P,NP).
+pnp([H|T],P,[H|NP]) :-
+	H mod 2 =:= 1,
+	pnp(T,P,NP).
+
+doposort(X,[],[X]).
+doposort(X, [H|T], [X,H|T]) :-
+	H >= X.
+doposort(X, [H|T], [H|OUT]) :-
+	H < X,
+	doposort(X,T,OUT).
+
+
+
+
+
+
