6138226a87b567bc1fa6795e72e254640cb26caf
matesik1995 <matesik1995@gmail.com>
2016-06-13 19:55:54 +0200

    Time measure
    Added time measurements for bruteforce

--- knapsack/sources/main.cpp
+++ knapsack/sources/main.cpp
@@ -1,12 +1,18 @@
 #include "../headers/knapsack.h"
 
+#include <ctime>
+
 int main(int argc, char const *argv[]){
 	clock_t begin, end;
 	if(argc < 3) return 1;
-	int x = atoi(argv[1]); //knapsack weight
-	int y = atoi(argv[2]); // elements count
-	Knapsack knapsack(x, y);
-
+	int capacity = atoi(argv[1]); //knapsack weight
+	int elementsCount = atoi(argv[2]); // elements count
+	Knapsack knapsack(capacity, elementsCount);
+	
+	begin= clock();
 	knapsack.bruteForce();
+	end= clock();
+	cout << "\nTime: " << (double(end - begin) / CLOCKS_PER_SEC) << endl;
+	
 	return 0;
 }
