a8c319861d7036ba94517ad65910b8e39bfaf47a
Druggist <m.najdora@gmail.com>
2016-12-21 10:34:48 +0100

    Pipies & shared memory

---
+++ pipes/ex_02.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+	int pdesk[2];
+
+	if (pipe(pdesk) == -1) {
+		perror("Creating pipe");
+		exit(1);
+	}
+
+	switch(fork()) {
+		case -1: // error creating process
+			perror ("Creating process");
+			exit(1);
+		case 0: // child
+			if(write(pdesk[1], "Hello", 7) == -1) {
+				perror("Writing to pipe");
+				exit(1);
+			}
+			exit(0);
+		default: { 
+			switch(fork()) {
+				case -1: // error creating process
+					perror ("Creating process");
+					exit(1);
+				case 0: // child
+					if(write(pdesk[1], "world!", 7) == -1) {
+						perror("Writing to pipe");
+						exit(1);
+					}
+					exit(0);
+				default: { 
+					char buf[10];
+					if(read(pdesk[0], buf, 10) == -1) {
+						perror ("Reading from pipe");
+						exit (1) ;
+					}
+					printf(" Read from pipe2 : %s \n" , buf);
+				}
+			}
+
+			char buf[15];
+			if(read(pdesk[0], buf, 15) == -1) {
+				perror ("Reading from pipe");
+				exit (1) ;
+			}
+			printf(" Read from pipe1 : %s \n" , buf);
+		}
+	}
+
+	return 0;
+}
